d9e86f53962627b909f357bcfc93359e7edfd550
zuhurul <33118938+zuhurul@users.noreply.github.com>
2019-10-20 22:48:55 +0700

    Create AppletSelamat.java

---
+++ BAB 2/AppletSelamat.java
@@ -0,0 +1,9 @@
+// Berkas: AppletSelamat.java
+
+import java.awt.Graphics;
+
+public class AppletSelamat extends java.applet.Applet {
+  public void paint(Graphics g) {
+    g.drawString("Selamat Belajar Java", 5, 20);
+  }
+}
