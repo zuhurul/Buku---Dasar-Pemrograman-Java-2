3302f401204cfeb3ba60074fed0e780ebc77f463
zuhurul <33118938+zuhurul@users.noreply.github.com>
2019-10-20 23:02:06 +0700

    Create LiteralBulat.java

---
+++ BAB 3/LiteralBulat.java
@@ -0,0 +1,9 @@
+// Berkas: LiteralBulat.java
+
+public class LiteralBulat {
+  public static void main(String[] args) {
+    System.out.println(17);
+    System.out.println(017);
+    System.out.println(0x17);
+  }
+}
